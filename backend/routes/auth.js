02c6124ab5c44dd5328ca6410548cb4197ecd6d4
Uday-Singh <udaysingh89999@gmail.com>
2026-04-24 20:14:08 +0530

    fixed model imports

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user');
 
 // ১. সাইন-আপ বা রেজিস্ট্রেশন
 router.post('/register', async (req, res) => {
